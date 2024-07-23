9229315c17f7817355d8ab47dd37d7456885cdf3
maxvun11 <maxvun@1utar.my>
2024-07-23 18:43:41 +0800

    Add files via upload

---
+++ cardata.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Car (
+    Car_Name VARCHAR(50),
+    Year INT,
+    Selling_Price FLOAT,
+    Present_Price FLOAT,
+    Kms_Driven INT,
+    Fuel_Type VARCHAR(10),
+    Seller_Type VARCHAR(10),
+    Transmission VARCHAR(10),
+    Owner INT
+);
+
